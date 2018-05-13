5ac069631360e140efbba10b187dc88503bd3594
Kei Fujikawa <fujikei22funk@gmail.com>
2018-05-13 22:40:55 +0900

    first commit

--- CloudAnchorSwift/ViewController.swift
+++ CloudAnchorSwift/ViewController.swift
@@ -9,72 +9,148 @@
 import UIKit
 import SceneKit
 import ARKit
+import ARCore
+import Firebase
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    private lazy var firebaseReference = Database.database().reference().child("hotspot_list")
+    private var gSession: GARSession!
+    private var fetchedAnchorIds: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
+        sceneView.session.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        gSession = try! GARSession(apiKey: Const.apiKey, bundleIdentifier: nil)
+        gSession.delegate = self
+        gSession.delegateQueue = DispatchQueue.main
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        observeAnchors()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(touchLocation, types: [.existingPlane, .existingPlaneUsingExtent, .estimatedHorizontalPlane])
+        
+        if let result = hitTestResults.first {
+            addAnchor(transform: result.worldTransform)
+        }
     }
+    @IBAction func fetchButtonTapped(_ sender: Any) {
+        resolveAnchors()
+    }
+    
+}
 
-    // MARK: - ARSCNViewDelegate
+// MARK: - Some Original Method
+extension ViewController {
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+    // fetch anchors from Firebase Database
+    private func observeAnchors() {
+        firebaseReference.observe(.value) { (snapshot) in
+            guard let value = snapshot.value as? [String : Any] else {
+                return
+            }
+            let list = value.values
+            list.forEach { value in
+                if let dic = value as? [String: Any],
+                    let anchorId = dic["hosted_anchor_id"] as? String {
+                    self.fetchedAnchorIds.append(anchorId)
+                }
+            }
+        }
+    }
+    
+    private func resolveAnchors(){
+        fetchedAnchorIds.forEach { id in
+            _ = try! gSession?.resolveCloudAnchor(withIdentifier: id)
+        }
+        fetchedAnchorIds.removeAll()
+    }
+    
+    // addAnchor to AR Space and share the anchor data to Google Cloud
+    private func addAnchor(transform: matrix_float4x4) {
+        let arAnchor = ARAnchor(transform: transform)
+        sceneView.session.add(anchor: arAnchor)
+        
+        do {
+            _ = try gSession.hostCloudAnchor(arAnchor)
+        } catch {
+            print(error)
+        }
+    }
+}
+
+// MARK: - <#ARSCNViewDelegate#>
+extension ViewController: ARSCNViewDelegate {
+    
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+        // Basicaly, you can get only ARPlaneAnchor,
+        // but this case, you can also get another anchor which is created by ARCore.
+        if !(anchor is ARPlaneAnchor) {
+            let scene = SCNScene(named: "art.scnassets/ship.scn")!
+            return scene.rootNode.childNode(withName: "ship", recursively: false)
+        }
+        return nil
     }
-*/
+}
+
+// MARK: - <#ARSCNViewDelegate#>
+extension ViewController: ARSessionDelegate {
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        do {
+            try gSession.update(frame)
+        } catch {
+            print(error)
+        }
+    }
+}
+
+// MARK: - <#GARSessionDelegate#>
+extension ViewController: GARSessionDelegate {
+    
+    func session(_ session: GARSession, didHostAnchor anchor: GARAnchor) {
+        let id = anchor.cloudIdentifier
+        firebaseReference.childByAutoId().child("hosted_anchor_id").setValue(id)
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    func session(_ session: GARSession, didFailToHostAnchor anchor: GARAnchor) {
+        print("didFailToHostAnchor")
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    func session(_ session: GARSession, didResolve anchor: GARAnchor) {
+        let arAnchor = ARAnchor(transform: anchor.transform)
+        sceneView.session.add(anchor: arAnchor)
+    }
+    
+    func session(_ session: GARSession, didFailToResolve anchor: GARAnchor) {
+        print("didFailToResolve")
     }
 }
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'CloudAnchorSwift' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'ARCore', '~> 1.2.0'
+  pod 'Firebase/Core', '~> 5.0.0'
+  pod 'Firebase/Database', '~> 5.0.0'
+
+end
